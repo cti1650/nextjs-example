5698bbc25d2fa96f5de7aa3f0be96dfeb9169591
cti1650 <cti1650@gmail.com>
2021-03-17 17:31:32 +0000

    add MemberTag

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import TitleMessage from '../src/components/qin/TitleMessage';
 import MembersTextbox from '../src/components/qin/Textbox';
 import Description from '../src/components/qin/Description';
 import Editarea from '../src/components/qin/Editarea';
+import MemberTag from '../src/components/qin/MemberTag';
+import CircleColor from '../src/components/color/CircleColor';
 
 export default function Home() {
   return (
@@ -13,6 +15,9 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div id="tags" className="text-white"></div>
+      <MemberTag user="cti"></MemberTag>
+      <MemberTag user="cti" pop={true}></MemberTag>
       <TitleMessage />
       <MembersTextbox />
       <Editarea />
--- src/components/qin/Textbox.jsx
+++ src/components/qin/Textbox.jsx
@@ -1,6 +1,47 @@
-import styles from "./Textbox.module.css";
+import {useEffect,useState} from "react";
+import MemberTag from './MemberTag';
+import * as styles from "./Textbox.module.css";
 
 function MembersTextbox(props) {
+    const [users,setUsers] = useState([]);
+    useEffect(()=>{
+        const tagsEl = document.getElementById("tags");
+        const textarea = document.getElementById("textarea");
+        
+        textarea.onkeyup = (e) => {
+            createTags(e.target.value);
+
+            if (e.key === "Enter") {
+                setTimeout(() => {
+                e.target.value = "";
+                }, 10);
+
+                randomSelect();
+            }
+        };
+    },users);
+    function createTags(input) {
+        const arr = [];
+        const tags = input
+            .split("@")
+            .filter((tag) => tag.trim() !== "")
+            .map((tag) => tag.trim());
+        // console.log(tags);
+        // if (tags.key > 6) {
+        //tagsEl.innerHTML = "";
+        tags.forEach((tag) => {
+            // const tagEl = document.createElement("MemberTag");
+            // tagEl.attributes('user',tag);
+            // tagEl.attributes('pop',false);
+            // tagsEl.appendChild(tagEl);
+            arr.push({'user':tag,'pop':false});
+        });
+        setUsers(arr);
+    }
+
+    function randomSelect() {
+        alert('select ok');
+    }
     return (
         <>
             <textarea
@@ -10,6 +51,15 @@ function MembersTextbox(props) {
                 placeholder="メンバーをドラッグ&ドロップ"
             >
             </textarea>
+            <div className="flex flex-row flex-wrap">
+                {
+                    users.map((item)=>{
+                        return (
+                            <MemberTag user={item.user} pop={item.pop}></MemberTag>
+                        );
+                    })
+                }
+            </div>
         </>
     )
 }
